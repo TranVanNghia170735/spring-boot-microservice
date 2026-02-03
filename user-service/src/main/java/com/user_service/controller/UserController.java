ee6fa691305f4f765a373803920b4dc0501bce18
NghiaTV3 <trannghi170735@gmail.com>
2026-02-03 11:15:22 +0700

    1.Tạo user-service

---
+++ user-service/src/main/java/com/user_service/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.user_service.controller;
+
+import com.user_service.model.User;
+import com.user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return  userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+
+}
