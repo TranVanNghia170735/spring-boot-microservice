cddce5451e89a940c2a72db243545e9fe2c46475
NghiaTV3 <trannghi170735@gmail.com>
2026-02-04 09:58:58 +0700

    2.Tạo Order service, Api-gateway, và config lại build.gradle theo mô hình microservice

---
+++ order-service/src/main/java/com/order_service/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.order_service.controller;
+
+
+import com.order_service.model.Order;
+import com.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderRepository orderRepository;
+
+    @PostMapping
+    public Order createOrder (@RequestBody Order order){
+        return orderRepository.save(order);
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders(){
+        return orderRepository.findAll();
+    }
+
+}
---
+++ order-service/src/main/java/com/order_service/model/Order.java
@@ -0,0 +1,22 @@
+package com.order_service.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private String product;
+    private Double price;
+}
